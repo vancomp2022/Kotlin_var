113a47322e85a8c7b1cacc1e0aa9edf35774b3da
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-29 16:57:51 -0300

    Adicionando arquivo bytebnk

---
+++ bytebank/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100.0 + 2.0
+    saldo += 200
+
+    var teste: String
+
+    Println(teste)
+
+    println("titular $titular")
+    println("numero da Conta $numeroConta")
+    println("saldo da conta $saldo")
+}
